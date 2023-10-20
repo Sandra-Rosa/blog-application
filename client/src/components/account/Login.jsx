65d2807879b0e9855f0db87998c46e1e53dd5805
Sandra-Rosa <sandraantony2002@gmail.com>
2023-10-20 21:35:22 +0530

    Finished untill signup/loginform

---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,73 @@
+import { Box, TextField, Button, styled, Typography } from "@mui/material";
+import { useState } from "react";
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/0.6);
+`;
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background: #fb6418;
+  color: white;
+  height: 48px;
+  border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+text-transform:none;
+background:#fff;
+color:#2874f0;
+height:48px;
+border-radius:2px
+box-shadow:0 2px 4px 0 rgb(0 0 0/20%);`;
+const Text = styled(Typography)`
+  color: #878787;
+  font-size: 16px;
+  text-align:center;
+`;
+const Login = () => {
+  const [account, toggleAccount] = useState("login");
+    const toggleSignup= ()=>{
+        account==='login'?toggleAccount('signup'):toggleAccount('login');
+    }
+  return (
+    
+    <Component>
+      <h1>Blogger</h1>
+      {account === "login" ? (
+        <Wrapper>
+          <TextField variant="standard" placeholder="Enter username" />
+          <TextField variant="standard" placeholder="Enter password" />
+          <LoginButton>Login</LoginButton>
+          <Text>OR</Text>
+          <SignupButton onClick={()=>toggleSignup()}>Create an account</SignupButton>
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <TextField variant="standard" placeholder="Enter name" />
+          <TextField variant="standard" placeholder="Enter username" />
+          <TextField variant="standard" placeholder="Enter password" />
+          <SignupButton>Sign Up</SignupButton>
+          <Text>OR</Text>
+          <LoginButton variant="contained" onClick={()=>toggleSignup()}>
+            Already have an account?
+          </LoginButton>
+        </Wrapper>
+      )}
+    </Component>
+  );
+};
+
+export default Login;
